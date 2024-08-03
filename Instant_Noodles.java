b7468c6e4df8fff3438cf6e774eb2c821a49e50e
Durga Prasad <147470036+23A91A6144@users.noreply.github.com>
2024-08-03 11:51:11 +0530

    import java.util.*; public class Nood{     public static void main(String[] args)     {         int a,b,c;         Scanner r = new Scanner(System.in);         a = r.nextInt();         b = r.nextInt();         c = a*b;         System.out.println(c);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Nood{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        Scanner r = new Scanner(System.in);
+        a = r.nextInt();
+        b = r.nextInt();
+        c = a*b;
+        System.out.println(c);
+    }
+}
