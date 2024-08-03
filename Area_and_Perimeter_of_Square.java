8b921179a63691ba9ab480dd0aee4bec7598201d
Durga Prasad <147470036+23A91A6144@users.noreply.github.com>
2024-08-03 11:33:18 +0530

    import java.util.*; public class Square {     public static void main(String[] args)     {         int x,a,b;         Scanner read = new Scanner(System.in);         x = read.nextInt();         a = x*x;         b = x*4;         System.out.println(a + " " + b);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Square {
+    public static void main(String[] args)
+    {
+        int x,a,b;
+        Scanner read = new Scanner(System.in);
+        x = read.nextInt();
+        a = x*x;
+        b = x*4;
+        System.out.println(a + " " + b);
+    }
+}
