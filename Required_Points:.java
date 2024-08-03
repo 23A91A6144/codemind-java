46548f9ce9623dc282976aff77c20e28047e2131
Durga Prasad <147470036+23A91A6144@users.noreply.github.com>
2024-08-03 11:47:28 +0530

    import java.util.*; public class Points{     public static void main(String[] args)     {         int a,b,c;         Scanner r = new Scanner(System.in);         a = r.nextInt();         b = r.nextInt();         c = b/a;         System.out.println(c);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Points{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        Scanner r = new Scanner(System.in);
+        a = r.nextInt();
+        b = r.nextInt();
+        c = b/a;
+        System.out.println(c);
+    }
+}
